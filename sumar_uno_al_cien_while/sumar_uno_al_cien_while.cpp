874a5e8da06645c436ee28d1ee4b73c72fbfa35e
Yamila Holik <shami.nair.holik@gmail.com>
2024-09-10 20:30:59 -0300

    ejercicio 18

---
+++ sumar_uno_al_cien_while/sumar_uno_al_cien_while.cpp
@@ -0,0 +1,12 @@
+/* EJERCICIO 18: Desarrollar un programa que permita Sumar los números del
+1 al 100 usando un bucle while */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	
+	
+	return 0;
+}
